380fcb591168915081a0e8821094f868679811be
furyaus <86178470+furyaus@users.noreply.github.com>
2021-07-06 10:03:07 +0000

    Changed inspire menu order

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ async def help(ctx):
     help_msg.add_field(name=".link:",value="This links your discord userid with your PUBG in-game name. ```.link furyaus```",inline=False)
     help_msg.add_field(name=".stats:",value="Retireve live PUBG API data for a single user and display. No stats, ranks or roles are changed or stored. ```.stats 0cker```",inline=False)
     help_msg.add_field(name=".mystats:",value="Queries PUBG API for your latest data, updates ranks, roles and stats which are stored via a JSON file. ```.mystats```",inline=False)
-    help_msg.add_field(name="Report issues: ",value="Head to github and create a new issue or feature request [https://github.com/furyaus/rankbot/issues](https://github.com/furyaus/rankbot/issues)",inline=False)
     help_msg.add_field(name=".inspire:",value="Responses with inspiration quotes, to really get you back on track for that chicken dinner.```.inspire```",inline=False)
+    help_msg.add_field(name="Report issues: ",value="Head to github and create a new issue or feature request [https://github.com/furyaus/rankbot/issues](https://github.com/furyaus/rankbot/issues)",inline=False)
     help_msg.timestamp = datetime.datetime.utcnow()
     await ctx.send(embed=help_msg)
 
