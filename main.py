c9cf8c194c7e5b3240aadb55b4c605da2498faa5
Peter Harley <peterharleymandurah@gmail.com>
2021-07-10 11:05:26 +1000

    added comments adjsuted userlist loop

--- main.py
+++ main.py
@@ -478,14 +478,16 @@ async def update():
         #Added all session infor to a new playerStats class
         playerStats = playerStatistics.statsCalc(player_id, request)
         #Def to update all user information from stats class
-        user_list = updateUserList(user_list, user, user_ign, player_id, playerStats, curr_punisher, curr_terminator, curr_general, curr_teamkiller)
+        user_list_na = updateUserList(user_list, user, user_ign, player_id, playerStats, curr_punisher, curr_terminator, curr_general, curr_teamkiller)
         if playerStats.pStats.new_rank != curr_rank:
             role = discord.utils.get(guild.roles, name=curr_rank)
-            member = await guild.fetch_member(user)
+            member = await guild.fetch_member(user_ign)
             await member.remove_roles(role)
             role = discord.utils.get(guild.roles, name=playerStats.pStats.new_rank)
             await member.add_roles(role)
-
+    
+    #Reset the user_list back to the new list after we iterate
+    user_list = user_list_na
     max_points = 0
     max_points_user = ''
     current_general = 'None'
