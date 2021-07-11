51fac496d5ccb6a4cf970ce1e1585164b70d2131
furyaus <86178470+furyaus@users.noreply.github.com>
2021-07-11 11:59:36 +0000

    Revert to fingers commit - on_member_join should be working

--- main.py
+++ main.py
@@ -219,8 +219,6 @@ async def on_member_remove(member):
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(botlog_channel)
-    after_id = member.id
-    member = await grabTargetUser(after_id)
     await discordAddRole('101 Club',member)
     response_msg = respmsg()
     response_msg.add_field(name="Server join", value=f"{member.name}", inline=False)
