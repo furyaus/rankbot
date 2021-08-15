0d3ffbbff6f40e322b2a75cf918e375974218056
furyaus <86178470+furyaus@users.noreply.github.com>
2021-08-15 08:11:08 +0000

    Cleanup Help

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ async def on_error(event, *args, **kwargs):
 async def help(ctx):
     response_msg = botHelper.respmsg("Help for Rank Bot","Rank Bot manages the roles, ranks and other stats for gamers in The 101 Club.")
     response_msg.add_field(name=".link",value="This links your discord userid with your PUBG in-game name. ```.link furyaus```",inline=False)
-    response_msg.add_field(name=".unlink",value="Removes any PUBG IGN linked with your discord account. ```.unlink```",inline=False)
+    response_msg.add_field(name=".unlink",value="Removes any PUBG IGN linked with your discord account. Allowing you to then .link another PUBG IGN. ```.unlink```",inline=False)
     response_msg.add_field(name=".stats",value="Retireve live PUBG API data for a single user and display. No stats, ranks or roles are changed or stored. ```.stats 0cker```",inline=False)
     response_msg.add_field(name=".mystats",value="Queries PUBG API for your latest data, updates ranks, roles and stats which are stored via a JSON file. ```.mystats```",inline=False)
     response_msg.add_field(name=".inspire",value="Responses with inspiration quotes, to really get you back on track for that chicken dinner.```.inspire```",inline=False)
